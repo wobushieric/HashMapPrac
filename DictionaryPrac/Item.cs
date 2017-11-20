67c7879ca0a9b9bf3281a379cd6fd6ef32216c5a
Eric Dai <dgxeric@gmail.com>
2017-11-20 15:45:32 -0600

    Added Item class

---
+++ DictionaryPrac/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPrac
+{
+    class Item : IComparable<Item>
+    {
+        public string Name { get; }
+        public int GoldPeices { get; }
+        public double Weight { get; }
+
+        public Item(string name, int goldPeices, double weight)
+        {
+            this.Name = name;
+            this.GoldPeices = goldPeices;
+            this.Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2}", this.Name, this.GoldPeices, this.Weight);
+        }
+
+        public int CompareTo(Item item)
+        {
+            return this.Weight.CompareTo(item.Weight);
+        }
+    }
+}
