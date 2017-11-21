8f7028d9e79cff6fc104bdb1e683c726144c9c2d
Eric Dai <dgxeric@gmail.com>
2017-11-20 22:38:28 -0600

    Added unit test class for StringKey class

---
+++ DictionaryPracTests/StringKeyTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DictionaryPrac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPrac.Tests
+{
+    [TestClass()]
+    public class StringKeyTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            string expected = "Unit Test";
+
+            StringKey stringKey = new StringKey(expected);
+
+            Assert.AreEqual(expected, stringKey.ToString());
+        }
+
+        [TestMethod()]
+        public void EqualsTestWithNull()
+        {
+            StringKey stringKey = new StringKey("Test");
+
+            Assert.AreEqual(false, stringKey.Equals(null));
+        }
+    }
+}
