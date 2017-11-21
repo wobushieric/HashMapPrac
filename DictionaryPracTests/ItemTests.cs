15cd2b4ba5d87aad6d4f4fb6cd44388e7e87b679
Eric Dai <dgxeric@gmail.com>
2017-11-20 22:22:18 -0600

    Added unit test for Item class

---
+++ DictionaryPracTests/ItemTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DictionaryPrac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPrac.Tests
+{
+    [TestClass()]
+    public class ItemTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Item item = new Item("Item", 5, 2.5);
+
+            string expected = "Item, " + 5 + ", " + 2.5;
+
+            Assert.AreEqual(expected, item.ToString());
+        }
+
+        [TestMethod()]
+        public void CompareToTest()
+        {
+            //Assert.Fail();
+        }
+    }
+}
